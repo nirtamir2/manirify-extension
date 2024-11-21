3275aab74434ea25064b11f88eb97e59d5badbd1
Nir Tamir <nirtamir2@gmail.com>
2024-11-21 14:40:32 +0200

    token

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  extensionApi: 'chrome',
-  modules: ['@wxt-dev/module-solid'],
+  extensionApi: "chrome",
+  manifest: {
+    permissions: ["aiLanguageModelOriginTrial"],
+    trial_tokens: [
+      "ApQdzvh4361CLQYNla9D/0DdsPOyiiNCg41gzHnVeAo2PsYluuUnf+dCxUtzhrlBtX4pab85WnZq5LDs+z0ouA4AAAB4eyJvcmlnaW4iOiJjaHJvbWUtZXh0ZW5zaW9uOi8vZmdja2ZrampjZ25pY29qb2hnY2Fnb2dnZm9mY2VkbWsiLCJmZWF0dXJlIjoiQUlQcm9tcHRBUElGb3JFeHRlbnNpb24iLCJleHBpcnkiOjE3NjA0ODYzOTl9",
+    ],
+  },
+  modules: ["@wxt-dev/module-solid"],
 });
